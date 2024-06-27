73f9eaddee18bc714ac7aa96f807fb9641f66bfa
Jennifer Onyeama <61958046+AfricanJay@users.noreply.github.com>
2024-06-27 03:06:05 +0100

    updating

---
+++ Training Linear Models/training_linear_models.py
@@ -0,0 +1,328 @@
+#First, let's import a few common modules, ensure MatplotLib plots figures inline and prepare a function to save the figures. 
+#We also check that Python 3.5 or later is installed (although Python 2.x may work, it is deprecated so we strongly recommend you use Python 3 instead), 
+#as well as Scikit-Learn ≥0.20.
+
+# Python ≥3.5 is required
+import sys
+assert sys.version_info >= (3, 5)
+
+# Scikit-Learn ≥0.20 is required
+import sklearn
+assert sklearn.__version__ >= "0.20"
+
+# Common imports
+import numpy as np
+import os
+
+# to make this notebook's output stable across runs
+np.random.seed(42)
+
+# To plot pretty figures
+%matplotlib inline
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.rc('axes', labelsize=14)
+mpl.rc('xtick', labelsize=12)
+mpl.rc('ytick', labelsize=12)
+
+# Where to save the figures
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "training_linear_models"
+IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID)
+os.makedirs(IMAGES_PATH, exist_ok=True)
+
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
+
+
+
+import numpy as np
+
+X = 2 * np.random.rand(100, 1)
+y = 4 + 3 * X + np.random.randn(100, 1)
+plt.plot(X, y, "b.")
+plt.xlabel("$x_1$", fontsize=18)
+plt.ylabel("$y$", rotation=0, fontsize=18)
+plt.axis([0, 2, 0, 15])
+save_fig("generated_data_plot")
+plt.show()
+
+
+X_b = np.c_[np.ones((100, 1)), X]  # add x0 = 1 to each instance
+theta_best = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y)
+theta_best
+array([[4.21509616],
+       [2.77011339]])
+X_new = np.array([[0], [2]])
+X_new_b = np.c_[np.ones((2, 1)), X_new]  # add x0 = 1 to each instance
+y_predict = X_new_b.dot(theta_best)
+y_predict
+array([[4.21509616],
+       [9.75532293]])
+plt.plot(X_new, y_predict, "r-")
+plt.plot(X, y, "b.")
+plt.axis([0, 2, 0, 15])
+plt.show()
+
+plt.plot(X_new, y_predict, "r-", linewidth=2, label="Predictions")
+plt.plot(X, y, "b.")
+plt.xlabel("$x_1$", fontsize=18)
+plt.ylabel("$y$", rotation=0, fontsize=18)
+plt.legend(loc="upper left", fontsize=14)
+plt.axis([0, 2, 0, 15])
+save_fig("linear_model_predictions_plot")
+plt.show()
+
+
+from sklearn.linear_model import LinearRegression
+
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+lin_reg.intercept_, lin_reg.coef_
+
+
+lin_reg.predict(X_new)
+
+
+theta_best_svd, residuals, rank, s = np.linalg.lstsq(X_b, y, rcond=1e-6)
+theta_best_svd
+
+np.linalg.pinv(X_b).dot(y)
+
+#Batch gradient Descent
+
+eta = 0.1  # learning rate
+n_iterations = 1000
+m = 100
+
+theta = np.random.randn(2,1)  # random initialization
+
+for iteration in range(n_iterations):
+    gradients = 2/m * X_b.T.dot(X_b.dot(theta) - y)
+    theta = theta - eta * gradients
+
+theta
+
+X_new_b.dot(theta)
+
+theta_path_bgd = []
+
+def plot_gradient_descent(theta, eta, theta_path=None):
+    m = len(X_b)
+    plt.plot(X, y, "b.")
+    n_iterations = 1000
+    for iteration in range(n_iterations):
+        if iteration < 10:
+            y_predict = X_new_b.dot(theta)
+            style = "b-" if iteration > 0 else "r--"
+            plt.plot(X_new, y_predict, style)
+        gradients = 2/m * X_b.T.dot(X_b.dot(theta) - y)
+        theta = theta - eta * gradients
+        if theta_path is not None:
+            theta_path.append(theta)
+    plt.xlabel("$x_1$", fontsize=18)
+    plt.axis([0, 2, 0, 15])
+    plt.title(r"$\eta = {}$".format(eta), fontsize=16)
+np.random.seed(42)
+theta = np.random.randn(2,1)  # random initialization
+
+plt.figure(figsize=(10,4))
+plt.subplot(131); plot_gradient_descent(theta, eta=0.02)
+plt.ylabel("$y$", rotation=0, fontsize=18)
+plt.subplot(132); plot_gradient_descent(theta, eta=0.1, theta_path=theta_path_bgd)
+plt.subplot(133); plot_gradient_descent(theta, eta=0.5)
+
+save_fig("gradient_descent_plot")
+plt.show()
+
+
+#Stochastic gradient descent
+
+theta_path_sgd = []
+m = len(X_b)
+np.random.seed(42)
+n_epochs = 50
+t0, t1 = 5, 50  # learning schedule hyperparameters
+
+def learning_schedule(t):
+    return t0 / (t + t1)
+
+theta = np.random.randn(2,1)  # random initialization
+
+for epoch in range(n_epochs):
+    for i in range(m):
+        if epoch == 0 and i < 20:                    # not shown in the book
+            y_predict = X_new_b.dot(theta)           # not shown
+            style = "b-" if i > 0 else "r--"         # not shown
+            plt.plot(X_new, y_predict, style)        # not shown
+        random_index = np.random.randint(m)
+        xi = X_b[random_index:random_index+1]
+        yi = y[random_index:random_index+1]
+        gradients = 2 * xi.T.dot(xi.dot(theta) - yi)
+        eta = learning_schedule(epoch * m + i)
+        theta = theta - eta * gradients
+        theta_path_sgd.append(theta)                 # not shown
+
+plt.plot(X, y, "b.")                                 # not shown
+plt.xlabel("$x_1$", fontsize=18)                     # not shown
+plt.ylabel("$y$", rotation=0, fontsize=18)           # not shown
+plt.axis([0, 2, 0, 15])                              # not shown
+save_fig("sgd_plot")                                 # not shown
+plt.show()                 
+
+
+#mini batch gradient descent
+
+theta_path_mgd = []
+
+n_iterations = 50
+minibatch_size = 20
+
+np.random.seed(42)
+theta = np.random.randn(2,1)  # random initialization
+
+t0, t1 = 200, 1000
+def learning_schedule(t):
+    return t0 / (t + t1)
+
+t = 0
+for epoch in range(n_iterations):
+    shuffled_indices = np.random.permutation(m)
+    X_b_shuffled = X_b[shuffled_indices]
+    y_shuffled = y[shuffled_indices]
+    for i in range(0, m, minibatch_size):
+        t += 1
+        xi = X_b_shuffled[i:i+minibatch_size]
+        yi = y_shuffled[i:i+minibatch_size]
+        gradients = 2/minibatch_size * xi.T.dot(xi.dot(theta) - yi)
+        eta = learning_schedule(t)
+        theta = theta - eta * gradients
+        theta_path_mgd.append(theta)
+theta
+array([[4.25214635],
+       [2.7896408 ]])
+theta_path_bgd = np.array(theta_path_bgd)
+theta_path_sgd = np.array(theta_path_sgd)
+theta_path_mgd = np.array(theta_path_mgd)
+plt.figure(figsize=(7,4))
+plt.plot(theta_path_sgd[:, 0], theta_path_sgd[:, 1], "r-s", linewidth=1, label="Stochastic")
+plt.plot(theta_path_mgd[:, 0], theta_path_mgd[:, 1], "g-+", linewidth=2, label="Mini-batch")
+plt.plot(theta_path_bgd[:, 0], theta_path_bgd[:, 1], "b-o", linewidth=3, label="Batch")
+plt.legend(loc="upper left", fontsize=16)
+plt.xlabel(r"$\theta_0$", fontsize=20)
+plt.ylabel(r"$\theta_1$   ", fontsize=20, rotation=0)
+plt.axis([2.5, 4.5, 2.3, 3.9])
+save_fig("gradient_descent_paths_plot")
+plt.show()
+
+
+
+#Polynomial regression
+
+
+import numpy as np
+import numpy.random as rnd
+
+np.random.seed(42)
+m = 100
+X = 6 * np.random.rand(m, 1) - 3
+y = 0.5 * X**2 + X + 2 + np.random.randn(m, 1)
+plt.plot(X, y, "b.")
+plt.xlabel("$x_1$", fontsize=18)
+plt.ylabel("$y$", rotation=0, fontsize=18)
+plt.axis([-3, 3, 0, 10])
+save_fig("quadratic_data_plot")
+plt.show()
+
+
+from sklearn.preprocessing import PolynomialFeatures
+poly_features = PolynomialFeatures(degree=2, include_bias=False)
+X_poly = poly_features.fit_transform(X)
+X[0]
+
+
+X_poly[0]
+
+lin_reg = LinearRegression()
+lin_reg.fit(X_poly, y)
+lin_reg.intercept_, lin_reg.coef_
+
+X_new=np.linspace(-3, 3, 100).reshape(100, 1)
+X_new_poly = poly_features.transform(X_new)
+y_new = lin_reg.predict(X_new_poly)
+plt.plot(X, y, "b.")
+plt.plot(X_new, y_new, "r-", linewidth=2, label="Predictions")
+plt.xlabel("$x_1$", fontsize=18)
+plt.ylabel("$y$", rotation=0, fontsize=18)
+plt.legend(loc="upper left", fontsize=14)
+plt.axis([-3, 3, 0, 10])
+save_fig("quadratic_predictions_plot")
+plt.show()
+
+#high degree polynomials
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+for style, width, degree in (("g-", 1, 300), ("b--", 2, 2), ("r-+", 2, 1)):
+    polybig_features = PolynomialFeatures(degree=degree, include_bias=False)
+    std_scaler = StandardScaler()
+    lin_reg = LinearRegression()
+    polynomial_regression = Pipeline([
+            ("poly_features", polybig_features),
+            ("std_scaler", std_scaler),
+            ("lin_reg", lin_reg),
+        ])
+    polynomial_regression.fit(X, y)
+    y_newbig = polynomial_regression.predict(X_new)
+    plt.plot(X_new, y_newbig, style, label=str(degree), linewidth=width)
+
+plt.plot(X, y, "b.", linewidth=3)
+plt.legend(loc="upper left")
+plt.xlabel("$x_1$", fontsize=18)
+plt.ylabel("$y$", rotation=0, fontsize=18)
+plt.axis([-3, 3, 0, 10])
+save_fig("high_degree_polynomials_plot")
+plt.show()
+
+#learning curves
+
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+
+def plot_learning_curves(model, X, y):
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=10)
+    train_errors, val_errors = [], []
+    for m in range(1, len(X_train) + 1):
+        model.fit(X_train[:m], y_train[:m])
+        y_train_predict = model.predict(X_train[:m])
+        y_val_predict = model.predict(X_val)
+        train_errors.append(mean_squared_error(y_train[:m], y_train_predict))
+        val_errors.append(mean_squared_error(y_val, y_val_predict))
+
+    plt.plot(np.sqrt(train_errors), "r-+", linewidth=2, label="train")
+    plt.plot(np.sqrt(val_errors), "b-", linewidth=3, label="val")
+    plt.legend(loc="upper right", fontsize=14)   # not shown in the book
+    plt.xlabel("Training set size", fontsize=14) # not shown
+    plt.ylabel("RMSE", fontsize=14)              # not shown
+lin_reg = LinearRegression()
+plot_learning_curves(lin_reg, X, y)
+plt.axis([0, 80, 0, 3])                         # not shown in the book
+save_fig("underfitting_learning_curves_plot")   # not shown
+plt.show()       
+
+from sklearn.pipeline import Pipeline
+
+polynomial_regression = Pipeline([
+        ("poly_features", PolynomialFeatures(degree=10, include_bias=False)),
+        ("lin_reg", LinearRegression()),
+    ])
+
+plot_learning_curves(polynomial_regression, X, y)
+plt.axis([0, 80, 0, 3])           # not shown
+save_fig("learning_curves_plot")  # not shown
+plt.show()                        # not shown
